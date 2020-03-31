d92b542e3aec851b6184a3573f6ea1d5485d0e86
Jay <wdj930729@naver.com>
2020-03-31 17:33:25 +0900

    Added view and template

--- polls/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.utils import timezone
-from .models import Post
-
-# Create your views here.
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'polls/post_list.html',{'posts':posts})
